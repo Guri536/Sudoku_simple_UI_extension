ebed80d5f6b700bd13e99cb973bbd6de8f0f4cb5
Guri536 <gursim3388@gmal.com>
2025-12-24 19:59:43 +0530

    added resizeable ui

--- script.js
+++ script.js
@@ -3,12 +3,6 @@ var asideEle = document.getElementById("aside");
 var tipsEle = document.getElementById("tips-articles-wrap");
 var footer = document.getElementsByTagName("footer")[0];
 
-var mouseEvent = new MouseEvent('click', {
-    view: window,
-    bubbles: true,
-    cancelable: true
-});
-
 if (asideEle)
     asideEle.remove();
 if (tipsEle)
@@ -18,11 +12,44 @@ if (footer)
 
 
 document.addEventListener('keydown', function (event) {
-    var pencil = document.querySelector('.game-controls-pencil svg');
+    var element = document.querySelector('.game-controls-pencil');
+
+    const pDown = new PointerEvent('pointerdown', { bubbles: true, cancelable: true, view: window, button: 0, buttons: 1, pointerId: 1, pointerType: 'mouse' });
+    const pUp   = new PointerEvent('pointerup',   { bubbles: true, cancelable: true, view: window, button: 0, buttons: 0, pointerId: 1, pointerType: 'mouse' });
+    const mDown = new MouseEvent('mousedown', { bubbles: true, cancelable: true, view: window, button: 0, buttons: 1 });
+    const mUp   = new MouseEvent('mouseup',   { bubbles: true, cancelable: true, view: window, button: 0, buttons: 0 });
+    const click = new MouseEvent('click',     { bubbles: true, cancelable: true, view: window, button: 0, buttons: 0 });
 
     if (event.key == '.') {
-        if (pencil)
-            console.log("What")
-            pencil.dispatchEvent(mouseEvent);
+        if (element){
+            element.dispatchEvent(pDown);
+            element.dispatchEvent(mDown);
+            element.dispatchEvent(pUp);
+            element.dispatchEvent(mUp);
+            element.dispatchEvent(click);
+        }
     }
 });
+
+function adjustLayout() {
+    var wrapper = document.querySelector('#body > div.container.site-content-wrapper');
+    
+    if (wrapper) {
+        // Option B: If you want a specific calculation based on window width:
+        var width = window.innerWidth;
+        if (width > 1350) {
+            wrapper.style.marginLeft = "-150px"; 
+        } else if (width > 1000) {
+            wrapper.style.marginLeft = "-20px"; 
+        }
+        else {
+            wrapper.style.marginLeft = "0px";
+        }
+    }
+}
+
+// Run once on startup to fix initial view
+adjustLayout();
+
+// Run automatically whenever the user resizes the browser
+window.addEventListener('resize', adjustLayout);
